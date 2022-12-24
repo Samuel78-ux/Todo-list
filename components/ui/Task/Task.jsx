71d4282cdb2caee7ad4b81613a51f319af152b08
Samuel78-ux <70773349+Samuel78-ux@users.noreply.github.com>
2022-12-24 17:18:35 +0100

    Update Task.jsx

--- components/ui/Task/Task.jsx
+++ components/ui/Task/Task.jsx
@@ -4,7 +4,7 @@ export default function Task(props) {
 	return (
 		<ul className='flex gap-4 p-2 border-b p-6 text-xl'>
 			<input type='checkbox'></input>
-			<li nClick={props.doneClicked}>{props.content}</li>
+			<li onClick={props.doneClicked}>{props.content}</li>
 			<div className='ml-auto'>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
